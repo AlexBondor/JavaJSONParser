5136b66e217c6f07b3bb344a45176f296f641cba
alexbondor <alexandru.viorel.bondor@gmail.com>
2016-06-05 12:09:47 +0300

    Setup done

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+/**
+ * @author Alex, 6/5/2016.
+ */
+import com.alibaba.fastjson.JSON;
+
+public class Main {
+    public static void main(String args[]) {
+//        User user = new User();
+//
+//        user.print();
+//
+//        String json = JSON.toJSONString(user);
+//
+//        System.out.println(json);
+//
+//        json = "{\"id\":\"3\",\"name\":\"alex\",\"test\":\"blabla\"}";
+//        System.out.println(json);
+//
+//        user = JSON.parseObject(json, User.class);
+//
+//        user.print();
+    }
+}
